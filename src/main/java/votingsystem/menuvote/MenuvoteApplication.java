a3b24014913436ce992ca970818bf73e7877eada
zamyslov@gmail.com <zamyslov@gmail.com>
2017-12-18 21:13:16 +0200

    spring boot pom

---
+++ src/main/java/votingsystem/menuvote/MenuvoteApplication.java
@@ -0,0 +1,12 @@
+package votingsystem.menuvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuvoteApplication.class, args);
+	}
+}
