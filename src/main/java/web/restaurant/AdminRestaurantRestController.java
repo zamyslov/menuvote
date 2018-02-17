cbc2b38c3eee7ff71c88e11de2ad4ddf2902e67d
zamyslov@gmail.com <zamyslov@gmail.com>
2018-02-17 12:32:55 +0200

    Restaurant rest controller

---
+++ src/main/java/web/restaurant/AdminRestaurantRestController.java
@@ -0,0 +1,46 @@
+package web.restaurant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import votingsystem.menuvote.model.Restaurant;
+import votingsystem.menuvote.service.RestaurantService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(AdminRestaurantRestController.REST_URL)
+public class AdminRestaurantRestController {
+    static final String REST_URL = "/rest/admin/restaurants";
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private RestaurantService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Restaurant> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void create(@Validated @RequestBody Restaurant restaurant) {
+        log.info("create {}", restaurant);
+        service.create(restaurant);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@Validated @RequestBody Restaurant restaurant) {
+        log.info("update {}", restaurant);
+        service.update(restaurant);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+}
