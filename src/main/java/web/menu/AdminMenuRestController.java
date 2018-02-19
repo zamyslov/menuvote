07fc81a9377c4f37253f64f3eb083dadf42112ac
zamyslov@gmail.com <194156567>
2018-02-19 16:11:26 +0200

    add AdminMenuController (in progress)

---
+++ src/main/java/web/menu/AdminMenuRestController.java
@@ -0,0 +1,44 @@
+package web.menu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import votingsystem.menuvote.model.Restaurant;
+import votingsystem.menuvote.service.MenuService;
+import votingsystem.menuvote.service.RestaurantService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(AdminMenuRestController.REST_URL)
+public class AdminMenuRestController {
+    static final String REST_URL = "/rest/admin/menu";
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MenuService service;
+
+//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void create(@Validated @RequestBody Restaurant restaurant) {
+//        log.info("create {}", restaurant);
+//        service.create(restaurant);
+//    }
+//
+//    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void update(@Validated @RequestBody Restaurant restaurant) {
+//        log.info("update {}", restaurant);
+//        service.update(restaurant);
+//    }
+//
+//    @DeleteMapping(value = "/{id}")
+//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+//    public void delete(@PathVariable("id") int id) {
+//        log.info("delete {}", id);
+//        service.delete(id);
+//    }
+
+}
